8e2fe42f58121cabf51a5ff4d53a3e2ed7b9cc7d
GeekyHim <work.himanshu1720@gmail.com>
2025-01-03 18:37:57 +0530

    1823. Find the Winner of the Circular Game

--- 1248. Nice Subarrays/Solution.java
+++ 1248. Nice Subarrays/Solution.java
@@ -1,23 +1,30 @@
 public class Solution {
-    public boolean IsOdd(int n){
-      if(n%2!=0) return true;
-      else return false;
-    }
-    public int numberOfSubarrays(int[] nums, int k) {
-        int total = 0;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            int count = k;
-            for (int j = i; j < len; j++) {
-                if(IsOdd(nums[j])==true) count--;
-                if(count==0){
-                    total++;
-                    break;
-                } 
-
+    public int atMostSubarrays(int[] nums, int k){
+        // solving using sliding windows
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        for(right=0;right<nums.length;right++){
+            if(nums[right]%2==1) k--;
+            while(k<0){
+                if(nums[left]%2==1) k++;
+                left++;
             }
+            count  +=  (right - left + 1);
+            // right - left + 1 se total sub-array milti hai 
+            // count += se vo pichle count me add ho jati hai 
         }
+        return count;
+    }
 
+
+    public int numberOfSubarrays(int[] nums, int k) {
+        int ans = atMostSubarrays(nums, k) - atMostSubarrays(nums, k-1);
+        // example 
+        // ek sliding window me zyada se zyada 3 honi chahiye aur esi kinti hogi -- A
+        // ek me max to max 2 honi chahiye and esi kitni hogi - B
+        // A-B sirf A wali bach jayegi
+        return ans;
     }
 
 }
